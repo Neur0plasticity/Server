a020da699e8e65e3566959781c59192f1dd73101
djtinkers <djtinkers365@gmail.com>
2019-11-07 00:45:25 -0800

    Routine Backup

--- spec.js
+++ spec.js
@@ -1,4 +1,4 @@
-let S = require('./server.class.js');
+let S = require('./lib/server.class.js');
 
 let s0 = new S()
 // Tests outlined
@@ -20,6 +20,52 @@ s0.program({
      * 
      * Ports
      * 
+     * Host
+     * 
+     * Backlog Queue
+     * 
+     * * // Fastify Listen Config
+     * host:        String,
+     * port:        Number,
+     * *exclusive: false,
+     * *readableAll: false,
+     * *writableAll: false,
+     * *ipv6Only: false
+     * 
+     * // Fastify instance
+     * 
+     * http2:               Boolean,         // default false
+     * https:               Boolean,         // default false    
+     * maxParamLength:      Number,          // default 100 chars
+     * bodyLimit:           Number,          // 1048576 (1MiB)
+     * onProtoPoisoning:    String,          // possible values are 'error', 'remove' and 'ignore'.
+     * ignoreTrailingSlash: Boolean,
+     * logger:              !!!HELP!!!!,
+     * disableRequestLogging: Boolean,        // default false
+     * serverFactory:       Function,      
+     * modifyCoreObjects:   Boolean,          // default false
+     * caseSensitive:       Boolean,          // default true
+     * requestIdHeader:     String,           // default 'request-id'
+     * reqIdLogLabel:       String,           // default 'reqId'
+     * genReqId:            String,           // lue of 'request-id' if provided or monotonically increasing integers
+     * trustProxy:          Boolean,          // default false
+     * pluginTimeout:       Number,           // default 10000 milliseconds
+     * queryStringParser:   Function,         // querystringParser: str => qs.parse(str)
+     * versioning:          Object,           // see documentation
+     * return503OnClosing:  Boolean,          // default true
+     * 
+     * ready:               Function,         // once server instance ready.
+     * close:               Function,         // once server closes
+     * 
+     * 
+     * use:     // Inserting Middleware
+     * 
+     * register: 
+     * 
+     * addHook: 
+     * 
+     * 
+     * 
      * Connections ... Servers/Databases/3rdparty, etc
      * 
      * Protocols allowed
@@ -27,7 +73,14 @@ s0.program({
      * use Fastify? Expressjs? 
      * 
      * verifications ... ip, session, authentications
+     * *********************************
      * 
+     * 
+     * 
+     * routing syntax
+     * 
+     * 
+     * **********************************
      * *** All configs for other properties
     },
     "routes": {
@@ -63,9 +116,6 @@ s0.program({
         // transport structure // json structure
 
 
-    },
-    "routing-syntax":     {
-        // organization change request ... move to routes
     },
     "params":             {
         // all variable inputs must be scrubbed
